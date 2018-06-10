b4e12297e14aa6ef313ed2252627aae4a51f9658
littlejellycat <littlejellycat@yandex.ru>
2018-06-10 18:05:16 +0300

    Sensors methods to test [1]

--- app/src/main/java/com/hackathon/a3davinci/SensorFragment.kt
+++ app/src/main/java/com/hackathon/a3davinci/SensorFragment.kt
@@ -14,13 +14,11 @@ import android.util.Log
 class SensorFragment : Fragment(), SensorEventListener {
     private var mSensorManager: SensorManager? = null
     private var mAccelerometer: Sensor? = null
-    private var mGyroscope: Sensor? = null
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mSensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mGyroscope = mSensorManager!!.getDefaultSensor(TYPE_GYROSCOPE);
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -28,13 +26,20 @@ class SensorFragment : Fragment(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-//        val lux = event.values[0]
+
         val coordinates = event.values
 
         Log.e("${event.sensor.type} sensors", coordinates.joinToString(", "))
-        // Do something with this sensor value.
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager!!.unregisterListener(this)
     }
 
 }
