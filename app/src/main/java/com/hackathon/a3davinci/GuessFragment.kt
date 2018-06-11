c020c362429733963c8ca84c16a9d94465c16186
teffery <asfdaf>
2018-06-11 06:39:40 +0300

    Timer 2.0

--- app/src/main/java/com/hackathon/a3davinci/GuessFragment.kt
+++ app/src/main/java/com/hackathon/a3davinci/GuessFragment.kt
@@ -11,6 +11,20 @@ import android.widget.TextView
 import java.util.*
 
 class GuessFragment : Fragment() {
+    companion object {
+        const val ARG_GUESS_GAME_ID = "arg_guess_game_id"
+        const val ARG_GUESS_USER_ID = "arg_guess_user_id"
+    }
+
+    fun newInstance(gameId: String, userId: String) : GuessFragment {
+        val args : Bundle = Bundle()
+        val fragment = GuessFragment()
+        args.putString(ARG_GUESS_GAME_ID, gameId)
+        args.putString(ARG_GUESS_USER_ID, userId)
+        fragment.arguments = args
+        return fragment
+    }
+
     private var canonManTextView: TextView? = null
     private var timer: TextView? = null
     private var image: ImageView? = null
@@ -25,8 +39,12 @@ class GuessFragment : Fragment() {
         return view
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startTimer(arguments?.getString(ARG_GUESS_GAME_ID), arguments?.getString(ARG_GUESS_USER_ID))
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        startTimer()
 
     }
 
@@ -35,11 +53,19 @@ class GuessFragment : Fragment() {
         TODO()
     }
 
-    private fun startTimer() {
+    private fun startTimer(gameId : String?, userId : String?) {
         var tm: Long = 30
         kotlin.concurrent.timer("timer", true, 0, 1000, {
             tm--
             timer?.text = tm.toString()
+            if(tm == 0.toLong()) { goToResult(gameId, userId)}
         })
     }
+
+    private fun goToResult(gameId : String?, userId : String?) {
+        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, ResultFragment().newInstance(gameId, userId))?.
+                addToBackStack(null)?.commit()
+    }
 }
+
+
